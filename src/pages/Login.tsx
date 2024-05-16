787f6ffae52c28a674625d6ef5aed49298ceb7df
Rodolfo Gama <rodolfo.gama2006@gmail.com>
2024-05-16 12:20:22 -0300

    Update Login Layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,59 +1,63 @@
 import React from "react";
-import { IonContent, 
-    IonPage, 
-    IonFooter, 
-    IonIcon,
-    IonCardHeader,
-    IonCardTitle, 
-    IonCard, 
-    IonCardContent, 
-    IonText, 
-    IonInput, 
-    IonButton,
+import {
+  IonContent,
+  IonIcon,
+  IonCardHeader,
+  IonCardTitle,
+  IonCard,
+  IonCardContent,
+  IonText,
+  IonInput,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonHeader,
+  IonToolbar,
+  IonButtons, 
+  IonBackButton, 
+  IonTitle
 } from "@ionic/react";
 import './Login.css';
-import {checkmark} from 'ionicons/icons';
+import { checkmark } from 'ionicons/icons';
 
 const Login: React.FC = () => {
-    return (
-        
-        <IonContent>
+  return (
+    <><IonHeader>
+      <IonToolbar>
+        <IonButtons slot="start">
+          <IonBackButton defaultHref="/Home"></IonBackButton>
+        </IonButtons>
+        <IonTitle>Back Button</IonTitle>
+      </IonToolbar>
+    </IonHeader>
 
+    <IonContent>
+        <IonGrid fixed={true} className="ion-width">
+          <IonRow class="ion-justify-content-center">
+            <IonCol sizeXl="5">
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardTitle className="ion-text-center" color={"success"}>FAZER LOGIN</IonCardTitle>
+                </IonCardHeader>
 
-        <IonCardHeader>
-            <div className="ion-text-center">
-              <IonCardTitle color={'success'}> 
-                <h1>GESTONE</h1> 
-              </IonCardTitle>
-            </div>
-          </IonCardHeader>
-        
-        <IonCard>
-          <IonCardContent>
-            
-            <div className='ion-text-center'>
-              <IonText>
-                <h1>Entrar no Gestone</h1>
-              </IonText>
-            </div>
-            
-            <IonInput label='Login:' placeholder='Email ou CPF' clearInput labelPlacement="floating" fill="solid"></IonInput>
+                <IonCardContent>
+                  <IonInput label='Login:' placeholder='Email ou CPF' clearInput labelPlacement="floating" fill="solid"></IonInput>
 
-            <IonInput label='Senha:' placeholder='Senha' type='password' clearInput labelPlacement='floating' fill='solid'></IonInput>
-            
+                  <IonInput label='Senha:' placeholder='Senha' type='password' clearInput labelPlacement='floating' fill='solid'></IonInput>
 
-            <IonButton expand='block' color={'success'}>
-            
-            <IonIcon slot='start' icon={checkmark}></IonIcon>
-            <IonText>
-                <h2>Entrar</h2>
-            </IonText>
-            </IonButton>
-          
-          </IonCardContent>
-        </IonCard>
-      </IonContent>
-    );
+                  <IonButton expand='block' color={'success'} className="ion-margin-top">
+                    <IonText> <h2>Entrar</h2> </IonText>
+                  </IonButton>
+
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+
+        </IonGrid>
+      </IonContent></>
+  );
 };
 
 export default Login;
